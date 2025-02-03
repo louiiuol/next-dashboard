711666994e8ed3a01e85fb509a70c6a95d0de304
Louis Godlewski <louis.godlewski@gmail.com>
2025-02-03 21:42:24 +0100

    [CORE] Introduce pages + links + fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lusitana, Poppins } from 'next/font/google';
+
+export const poppins = Poppins({weight: '500', subsets: ['latin']});
+export const lusitana = Lusitana({weight: "400", subsets: ['latin']})
